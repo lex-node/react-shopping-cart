f4d14389e24dab29b1616364dfa2408752ff9c54
triggering-event <gabriel.j.shapiro@gmail.com>
2019-09-19 13:23:44 -0400

    confirms completion of step 6 + minor clean-up

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -29,13 +29,3 @@ const ShoppingCart = () => {
 export default ShoppingCart;
 
 
-/***STEP 5 - Providing data with CartContext**
-
-
-
- - Now that we're providing our cart data, we can start to refactor our `Navigation` and `ShoppingCart` components.
-
- - Let's start with our `ShoppingCart` component first. Go ahead and refactor the `ShoppingCart` route to no longer use render props. This will throw us an error, but we'll be able to resolve it quickly.
-
- - While were at it let's go ahead and remove the props from our navigation as well.
- */
