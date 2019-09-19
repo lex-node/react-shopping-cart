07549a0f3cd958c7bdd720ed687d7e38721a429c
triggering-event <gabriel.j.shapiro@gmail.com>
2019-09-19 11:49:24 -0400

    refines context provider component and refactors App.js for contextprovider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, {useState} from 'react';
+import {Route} from 'react-router-dom';
+import {ProductContextProvider} from "./contexts/ProductContext";
 
 // Components
 import Navigation from './components/Navigation';
@@ -8,35 +8,27 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		setCart([...cart, item]);
-	};
+    const addItem = item => {
+        setCart([...cart, item]);
+    };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+    return (
+        <ProductContextProvider>
+            <div className="App">
+                <Navigation cart={cart}/>
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+                {/* Routes */}
+                <Route exact path="/" component={Products}/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-	);
+                <Route
+                    path="/cart"
+                    render={() => <ShoppingCart cart={cart}/>}
+                />
+            </div>
+        </ProductContextProvider>
+    );
 }
 
 export default App;
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,12 +1,13 @@
 import {createContext, useState} from 'react';
+import data from '.,/data';
 
 export const ProductContext = createContext();
 
 export const ProductContextProvider = props => {
-    const [products, setProducts] = useState();
+    const [products, setProducts] = useState(data);
 
     return (
-        <ProductContext.Provider>
+        <ProductContext.Provider value={[products, setProducts]}>
             {props.children}
         </ProductContext.Provider>
     )
